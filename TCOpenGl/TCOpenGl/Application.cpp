8104c3f0e189f260941b47d9219635120f973a95
PC88 <32823980+PC88@users.noreply.github.com>
2019-01-29 15:35:18 +0000

    vid 4 commit

--- TCOpenGl/TCOpenGl/Application.cpp
+++ TCOpenGl/TCOpenGl/Application.cpp
@@ -26,13 +26,34 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	glewInit();
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Error!" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positons[6] // array of float to define out vertexes - PC
+	{
+	 -0.5f, -0.5f,
+	  0.0f,  0.5f,
+	  0.5f, -0.5f
+	};
+
+
+
+	unsigned int buffer;
+	glGenBuffers(1, &buffer); // this generates a buffer and gives us back a unique ID - PC // create buffer
+	glBindBuffer(GL_ARRAY_BUFFER, buffer); // bind or select that buffer - PC
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positons, GL_STATIC_DRAW); // 6 times the amount of vertexes held in array "positions" - PC // define size of buffer
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3); // Draw call - PC
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
