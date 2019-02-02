1c290f90dd3a3d8bd8c65b5f36afd278d3e81153
PC88 <32823980+PC88@users.noreply.github.com>
2019-02-02 03:40:31 +0000

    check

--- TCOpenGl/TCOpenGl/Application.cpp
+++ TCOpenGl/TCOpenGl/Application.cpp
@@ -102,8 +102,6 @@ int main(void)
 		return -1;
 	}
 
-	
-
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
